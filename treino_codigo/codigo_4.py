ecd98fc1bb1ea2c9554384ef4e49e8564ee9a5e5
DavidBratz <davidbratz2007@gmail.com>
2026-03-31 12:13:44 -0300

    codigo_4.py
    add codigo_4

---
+++ treino_codigo/codigo_4.py
@@ -0,0 +1,7 @@
+nota_1 = float(input("DIGITE A NOTA DO PRIMEIRO BIMESTRE:"))
+nota_2 = float(input("DIGITE A NOTA DO SEGUNDO BIMESTRE:"))
+nota_3 = float(input("DIGITE A NOTA DO TERCEIRO BIMESTRE:"))
+nota_4 = float(input("DIGITE A NOTA DO QUARTO BIMESTRE:"))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4 
+print("SUA MÉDIA É:", media)
