ecc38d487acc14a13d88ee66de138d60a39e0491
DavidBratz <davidbratz2007@gmail.com>
2026-03-31 09:45:53 -0300

    Delete codigo_da_nota.py

--- codigo_da_nota.py
+++
@@ -1,7 +0,0 @@
-media = (int(input("DIGITE SUA MÉDIA :")));
-if media >= 7 :
-    print("voce esta aprovado")
-elif media >= 4 :
-    print("voce esta de final")
-else :
-    print("voce esta reprovado") 
