cdcae3a1b1063c054d12aa6e6abb894b6d880814
DavidBratz <davidbratz2007@gmail.com>
2026-04-01 19:11:52 -0300

    codigo_10.py
    add codigo_10

---
+++ treino_codigo/codigo_10.py
@@ -0,0 +1,4 @@
+temperatura_c = float(input("INFORME A TEMPERATURA EM CELSIUS:"))
+
+temperatura_f = (9*temperatura_c) / 5 + 32
+print (f"SUA TEMPERATURA EM FAHRENHEIT É IGUAL À: {temperatura_f: .2f}")
