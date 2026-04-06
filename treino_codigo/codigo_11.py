05f2a868a09bed120d82ef799d541063525d0889
DavidBratz <davidbratz2007@gmail.com>
2026-04-06 16:04:33 -0300

    codigo_11.py
    add codigo_11

---
+++ treino_codigo/codigo_11.py
@@ -0,0 +1,12 @@
+num_1 = int(input("INSIRA O PRIMEIRO NÚMERO INTEIRO:"))
+num_2 = int(input("INSIRA O SEGUNDO NÚMERO INTEIRO:"))
+num_3 = float(input("INSIRA O NÚMERO REAL:"))
+
+primeira_conta = (num_1 * 2) * (num_2 / 2)
+print("O RESULTADO DA PRIMEIRA CONTA É:",primeira_conta)
+
+segunda_conta = (num_1 * 3) + num_3
+print("O RESULTADO DA SEGUNDA CONTA É:" , segunda_conta)
+
+terceira_conta = (num_3 * num_3 * num_3)
+print("O RESULTADO DA TERCEIRA CONTA É:" , terceira_conta)
