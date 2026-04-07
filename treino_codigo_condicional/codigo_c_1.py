de1f9b8130454355d5b554588a56d330695da538
DavidBratz <davidbratz2007@gmail.com>
2026-04-06 22:11:53 -0300

    codigo_c_1.py
    add codigo_c_1

---
+++ treino_codigo_condicional/codigo_c_1.py
@@ -0,0 +1,7 @@
+num_1 = float(input("INFORME O PRIMEIRO NÚMERO:"))
+num_2 = float(input("INFORME O SEGUNDO NÚMERO:"))
+
+if num_1 > num_2 :
+    print("O NUMERO MAIOR É:", num_1)
+else:
+    print("O NUMERO MAIOR É:", num_2)
