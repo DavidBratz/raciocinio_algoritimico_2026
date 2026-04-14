dd33a264a9ebf880875196ac14a54c578522e2c0
DavidBratz <davidbratz2007@gmail.com>
2026-04-14 11:37:19 -0300

    codigo_1_while_af.py
    add codigo_1_while_af.py

---
+++ atividade_formativa/codigo_1_while_af.py
@@ -0,0 +1,6 @@
+#----------------- TABUADA DO CINCO USANDO WHILE ------------------------------------------------------
+contador = 0 
+print("ESSA É A TABUADA DO CINCO:")
+while contador <= 10:
+    print(f"5 VEZES(X) {contador} = {5 * contador}")
+    contador += 1 
