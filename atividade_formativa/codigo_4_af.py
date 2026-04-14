060db024564edef18c65f2d8a4e06232d2fe0d32
DavidBratz <davidbratz2007@gmail.com>
2026-04-14 15:22:50 -0300

    codigo_4_af.py
    add codigo_4_af.py

---
+++ atividade_formativa/codigo_4_af.py
@@ -0,0 +1,7 @@
+#----------------------- CONTAGEM REGRESSIVA ------------------------------------------------
+numero = int(input("DIGITE UM NÚMERO: "))
+contador = numero
+
+while contador >= 0:
+    print(f"CONTAGEM: {contador}")
+    contador -= 1
