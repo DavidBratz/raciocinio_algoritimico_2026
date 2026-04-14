9b49037e882f7fdcbce519e6567dcb099cc549ad
DavidBratz <davidbratz2007@gmail.com>
2026-04-14 15:20:59 -0300

    codigo_3_af.py
    add codigo_3_af.py

---
+++ atividade_formativa/codigo_3_af.py
@@ -0,0 +1,5 @@
+#----------------------- TABUADA DE UM NÚMERO ------------------------------------------------
+numero = int(input("DIGITE UM NÚMERO: "))
+
+for i in range(1, 11):
+    print(f"{numero} X (VEZES) {i} = {numero * i}")
