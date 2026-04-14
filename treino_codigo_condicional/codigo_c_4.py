fd6e32a87c6474e04056b3b75968f05b000dea67
DavidBratz <davidbratz2007@gmail.com>
2026-04-14 16:28:16 -0300

    codigo_c_4.py
     add codigo_c_4.py

---
+++ treino_codigo_condicional/codigo_c_4.py
@@ -0,0 +1,14 @@
+letra = input("DIGITE QUALQUER LETRA DO ALFABETO:")
+if letra == "a":
+    print(f"A LETRA {letra} É UMA VOGAL")
+elif letra == "e":
+    print(f"A LETRA {letra} É UMA VOGAL")
+elif letra == "i":
+    print(f"A LETRA {letra} É UMA VOGAL")
+elif letra == "o":
+    print(f"A LETRA {letra} É UMA VOGAL")
+elif letra == "u":
+    print(f"A LETRA {letra} É UMA VOGAL")
+else:
+    print(f"A LETRA {letra} É UMA CONSOANTE")
+  
