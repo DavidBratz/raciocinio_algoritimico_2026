d895d4886b2b2cf3b06c83d682108d4a9dfdc1af
DavidBratz <davidbratz2007@gmail.com>
2026-04-14 16:06:52 -0300

    codigo_c_3.py
    add codigo_c_3.py

---
+++ treino_codigo_condicional/codigo_c_3.py
@@ -0,0 +1,7 @@
+letra = (input("DIGITE F PARA FEMININO OU M PARA MASCULINO:"))
+if letra == "f":
+    print("PARABENS,VOCÊ É UMA MULHER !!!")
+elif letra == "m":
+    print("PARABENS,VOCÊ É UM HOMEM !!!")
+else:
+    print("SEXO INVÁLIDO, TENTE NOVAMENTE")
