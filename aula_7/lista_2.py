d38cd48634b6326ddd0a0815cdbdc46ef251d397
DavidBratz <davidbratz2007@gmail.com>
2026-04-28 09:41:39 -0300

    lista_2.py
    add lista_2.py

---
+++ aula_7/lista_2.py
@@ -0,0 +1,8 @@
+a = []
+for i in range(10):
+    numero = int(input(f"Digite o {i+1}º número inteiro: "))
+    a.append(numero)
+
+print("Dados da lista a:")
+for elemento in a:
+    print(elemento)
