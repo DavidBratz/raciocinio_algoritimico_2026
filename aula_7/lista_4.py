e8f69ab1e47dc0a231a1fa8aabe742b6d7c33619
DavidBratz <davidbratz2007@gmail.com>
2026-04-28 11:12:59 -0300

    lista_4.py
    UP  lista_4.py

--- aula_7/lista_4.py
+++ aula_7/lista_4.py
@@ -5,12 +5,16 @@ na_media = 0
 acima_media = 0
 for i in range(quantidade):
     nota = int(input(f"Informe a nota do {1 + i}º aluno:"))
+    while nota > 100:
+        print("Nota inválida! Digite um número até 100.")
+        nota = int(input(f"Informe novamente a nota do {i + 1}º aluno: "))
     if nota < 60:
         abaixo_media += 1
     elif nota > 60:
         acima_media +=1
     else:
         na_media += 1
+
 print(f"A quantidade de alunos acima da média é: {acima_media}")
 print(f"A quantidade de alunos na média é: {na_media}")
 print(f"A quantidade de alunos abaixo da média é: {abaixo_media}")
