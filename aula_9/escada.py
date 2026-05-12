1400d2f5fa531ebe3629ae1474cd0272b50fd2b3
DavidBratz <davidbratz2007@gmail.com>
2026-05-12 09:10:49 -0300

    escada.py
    up escada.py

--- aula_9/escada.py
+++ aula_9/escada.py
@@ -1,1 +1,7 @@
-s
+n = int(input("Digite o valor do n(MAIOR QUE 0 E QUE SEJA UM NÚMERO INTEIRO OU SEJA SEM VÍRGULAS):"))
+
+
+for i in range(1, n + 1):
+    espaços = (n - i)
+    controle = (i * 2)
+    print(" " * espaços + "X" * controle)
