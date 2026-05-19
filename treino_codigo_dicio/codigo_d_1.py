e5bef46556e7033ab16413386f25958613ed3c57
DavidBratz <davidbratz2007@gmail.com>
2026-05-19 11:03:26 -0300

    codigo_d_1.py
    add codigo_d_1.py

--- treino_codigo_dicio/codigo_d_1.py
+++ treino_codigo_dicio/codigo_d_1.py
@@ -1,1 +1,9 @@
-ss
+personagens = {
+    'Naruto': ['Sábio', 'Kurama', 'Barion', 'Sábio dos seis caminhos', 'MK'],
+    'Sasuke': ['Marca da maldição', 'Sábio dos seis caminhos', 'Sharingan', 'Mangekyo Sharingan', 'Susano'],
+    'Sakura': ['Byakugou', 'Base', 'Ninja médica', 'Cem curas', 'Invocação']
+}
+
+print("Modos do Naruto:",personagens['Naruto'])
+print("Modos do Sasuke:",personagens['Sasuke'])
+print("Modos da Sakura:",personagens['Sakura'])
